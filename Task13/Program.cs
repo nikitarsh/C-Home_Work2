fe3587304ce3a63c160ee92870d7ae79f3db0e47
nikitarsh <nikitaroshmakov@gmail.com>
2022-08-12 16:58:37 +0400

    add task 15

---
+++ Task13/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Напишите программу, которая выводит третью цифру
+заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+
+*/
+
+
+Console.Write("Введите число: ");
+
+int Num = Convert.ToInt32(Console.ReadLine());
+
+string NumStr = Convert.ToString(Num);
+
+if (NumStr.Length > 2)
+
+{
+
+Console.WriteLine("третья цифра -> " + NumStr[2]);
+
+}
+
+else
+
+{
+  Console.WriteLine("-> третьей цифры нет");
+
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру,
+обозначающую день недели, и проверяет,
+является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+int DayOfWeek = new int();
+Console.WriteLine("Введите день недели: ");
+
+DayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+if (DayOfWeek == 6 || DayOfWeek == 7)
+{
+    Console.WriteLine("Этот день недели выходной");
+} 
+else if (DayOfWeek < 1 || DayOfWeek > 7) {
+    Console.WriteLine("В неделе всего 7 дней! ");
+
+}
+else Console.WriteLine("Этот день недели рабочий");
+
+
