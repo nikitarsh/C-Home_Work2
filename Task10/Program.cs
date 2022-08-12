cc1052241adbb66375f1039bcc30dcb2db429eb6
nikitarsh <nikitaroshmakov@gmail.com>
2022-08-12 15:46:17 +0400

    add task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая принимает на вход 
+трёхзначное число
+и на выходе показывает 
+вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int number = new Random().Next(100, 999);
+
+int result = (number % 100) / 10;
+
+Console.WriteLine(number);
+
+Console.WriteLine(result);
